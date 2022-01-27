9f58607075e5aaad6c54764b43a2d2d0eb6be73f
NhuYLe <lenhuy@sheridancollege.ca>
2022-01-26 22:10:14 -0500

    Reverting the array changes

--- src/student_w22_fri/StudentList.java
+++ src/student_w22_fri/StudentList.java
@@ -1,8 +1,16 @@
-
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package student_w22_fri;
+
+/**
+ *
+ * @author lenhu
+ */
 public class StudentList {
     public static void main(String[] args){
-     Student[] studentList = new Student[4];
+     Student[] studentList = new Student[3];
      studentList[0] = new Student("s1","Nancy");
       studentList[1] = new Student("s2","Tim");
        studentList[2] = new Student("s3","John");
